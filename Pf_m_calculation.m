455816104822bb72c8bdbc33c70ccb87d6db437c
Your Name <sanidhay@ece.iisc.ernet.in>
2016-07-05 13:57:30 +0530

    2nd

---
+++ Pf_m_calculation.m
@@ -0,0 +1,31 @@
+clc;
+close all; clear all;
+
+n=100;
+M=1;
+p=0.5;
+g =[];
+f=[];
+h=[];
+
+for i=1:n
+    a= factorial(n);
+    b = factorial(i) * factorial(n-i);
+    c= a/b;
+    g(i) = ((p/(1-p))^i) * c;
+end
+d = sum(g);
+
+for j=1:n
+    
+    for k=1:j
+        f(k) = (1 - 2^((k-1)-(n-M)));
+    end
+    e= prod(f);
+    
+    h(j) = ((p/(1-p))^j) * e * (factorial(n)/(factorial(j)*factorial(n-j)));
+end
+
+l=sum(h);
+
+P_f =((1-p)^n)*(d-l);
